25b76c1d94c72337fd8bbb21f948a12313fb5662
ccracker360 <ccracker360@gmail.com>
2025-06-23 19:47:38 +0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,18 @@
+import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+
+TOKEN = "7761279374:AAFoMsGoXFL9XV3cb9w1Yk_ft0IeYvsM_2E"
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = InlineKeyboardMarkup()
+    markup.add(InlineKeyboardButton("OPEN", web_app=WebAppInfo(url="https://telegram-mines-webapp1.vercel.app/index.html")))
+    bot.send_photo(
+        message.chat.id,
+        photo="https://i.imgur.com/kH1IxU1.png",
+        caption="🎮 O'yinlar markaziga xush kelibsiz!",
+        reply_markup=markup
+    )
+
+bot.polling()
